28530f54eccd70053bbc1d8986f179135bf85717
YeisonEng <yeison.eng@gmail.com>
2014-08-24 21:11:36 -0500

    Update libs

--- p32/lib_template32.c
+++ p32/lib_template32.c
@@ -2,7 +2,7 @@
 /*useless library that only turns on and off a pin*/
 
 #include <digitalw.c>
-#include <delay.c>
+#include <delayms.c>
 
 void blinkled_32(int pin, int repeat, unsigned int delay_on, unsigned int delay_off)
 {
--- p8/lib_template.c
+++ p8/lib_template.c
@@ -2,9 +2,7 @@
 /*useless library that only turns on and off a pin*/
 
 #include <digitalw.c>
-#include <delay.c>
-
-
+#include <delayms.c>
 
 void blinkled(int pin, int repeat, unsigned int delay_on, unsigned int delay_off)
 {
